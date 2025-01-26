2b0f2b6f676907b0a3c84bc5a83dc03977f7aead
Игорь Царёв <tsarov88@gmail.com>
2025-01-26 16:02:43 +0200

    orderform.html;ordercontroller.java;home.html

--- src/main/java/com/example/springinaction/takoapp/web/OrderController.java
+++ src/main/java/com/example/springinaction/takoapp/web/OrderController.java
@@ -1,16 +1,29 @@
 package com.example.springinaction.takoapp.web;
 
+import com.example.springinaction.takoapp.TacoOrder;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.support.SessionStatus;
 
+@Slf4j
 @Controller
 @RequestMapping("/orders")
+@SessionAttributes("tacoorder")
 public class OrderController {
 
     @GetMapping("/current")
     public String orderForm(){
         return "orderForm";
+    }
+
+    @PostMapping
+    public String processOrder(TacoOrder tacoOrder,
+                               SessionStatus sessionStatus){
+        log.info("Order submitted:{}", tacoOrder);
+        sessionStatus.setComplete();
+
+        return "redirect:/";
     }
 
 }
